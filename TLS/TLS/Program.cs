69c17a0965a8d217b01b4fa02693df94a6c4ee04
JackMead <Jack.Mead@softwire.com>
2017-09-06 10:55:02 +0100

    Dictionary added to track all tls found

--- TLS/TLS/Program.cs
+++ TLS/TLS/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace TLS
@@ -10,6 +12,52 @@ namespace TLS
     {
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.run();
+        }
+
+        private void run()
+        {
+            string text = getText();
+
+            string pattern = @"\w\w\w";
+            findTLS(text,pattern);
+
+            
+
+        }
+
+        private void findTLS(string text, string pattern)
+        {
+            
+            Regex rgx = new Regex(pattern, RegexOptions.IgnoreCase);
+            Dictionary<string, int> tlsDictionary = new Dictionary<string, int> {};
+            MatchCollection matches = rgx.Matches(text);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Value);
+                if (tlsDictionary.ContainsKey(match.Value))
+                {
+                    tlsDictionary[match.Value] += 1;
+                }
+                else
+                {
+                    tlsDictionary.Add(match.Value, 1);
+                }
+            }
+        
+
+            Console.WriteLine("The TLS tra appears " + tlsDictionary["tra"] + " times");
+        }
+
+        private string getText()
+        {
+            string path = "SampleText.txt";
+
+            // Open the file to read from.
+            string readText = File.ReadAllText(path);
+            return readText;
         }
     }
 }
